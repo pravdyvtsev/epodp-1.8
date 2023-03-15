fcae0e4ac24f3bf418dc3bf3c30c820ab3417bdf
Pavlo Pravdyvtsev <54671242+pravdyvtsev@users.noreply.github.com>
2023-03-15 11:23:12 +0200

    Add files via upload

---
+++ data/slide5.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{0698A46F-7FDC-4581-8516-793A04E057AB}'];loadHandler&&loadHandler(4, '<div id="spr0_3043f5f"><div id="spr1_3043f5f" class="kern slide"><img id="img0_3043f5f" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_3043f5f" class="kern slide"></div></div>', '{"s":[]}');})();
